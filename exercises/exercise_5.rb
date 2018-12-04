315ac093e543a4d7658f12e34c96e273631e5104
Chris <chrisstanarsenault@gmail.com>
2018-12-04 13:45:52 -0500

    finished excerise5: Calculations

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,6 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+@total_rev = Store.sum('annual_revenue')
+@avg_rev = @total_rev / Store.count
+puts Store.where('annual_revenue > 1000000').count
