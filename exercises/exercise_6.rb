ecf61a6da8bff575b553bf85c6c5664059f1591f
Chris <chrisstanarsenault@gmail.com>
2018-12-04 14:28:52 -0500

    finished exercise6: added new employees

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,15 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+
+@store1.employees.create!(first_name: "Luke", last_name: "SkyWalker", hourly_rate: 20)
+@store1.employees.create!(first_name: "Boba", last_name: "Fett", hourly_rate: 20)
+@store1.employees.create!(first_name: "Jabba", last_name: "The Hutt", hourly_rate: 40)
+
+@store2.employees.create!(first_name: "Anakin", last_name: "SkyWalker", hourly_rate: 30)
+@store2.employees.create!(first_name: "Leia", last_name: "SkyWalker", hourly_rate: 20)
+@store2.employees.create!(first_name: "Han", last_name: "Solo", hourly_rate: 25)
+
+puts @store2.employees.count
+
+
